c484767b4bf3149d019d7eb809c1191c7be7accd
Michael Ekstrand <md@ekstrandom.net>
2016-09-05 19:50:01 -0600

    more logical parameter order

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const mkdirp = require('mkdirp');
 const path = require('path');
 const async = require('async');
 
-function makeAcmeResponse(baseDir, acme, callback) {
+function makeAcmeResponse(acme, baseDir, callback) {
   var dir = path.join(baseDir, '.well-known', 'acme-challenge');
   var outfn = path.join(baseDir, '.well-known', 'acme-challenge', acme.challenge);
   async.series([
@@ -13,7 +13,7 @@ function makeAcmeResponse(baseDir, acme, callback) {
   ], callback);
 }
 
-function loadAndMakeResponse(baseDir, acmeFile, callback) {
+function loadAndMakeResponse(acmeFile, baseDir, callback) {
   const yaml = require('js-yaml');
 
   fs.access(acmeFile, fs.R_OK, (err) => {
@@ -24,7 +24,7 @@ function loadAndMakeResponse(baseDir, acmeFile, callback) {
         (cb) => fs.readFile(acmeFile, 'utf8', cb),
         (txt, cb) => {
           let acme = yaml.safeLoad(txt);
-          makeAcmeResponse(baseDir, acme, cb);
+          makeAcmeResponse(acme, baseDir, cb);
         }
       ], callback);
     }
